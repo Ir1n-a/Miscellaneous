1fafb9d77de45b2f8d05d322d64b17deb1b494c0
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-05-01 02:09:39 +0300

    updated columns and lines reading

--- columns conjoining.jl
+++ columns conjoining.jl
@@ -2,22 +2,31 @@ using CSV
 using NativeFileDialog
 using Tables
 
-function table(name,n)
+function table(name,n,lim_lin,lim_col)
     f2 = pick_folder()
     r = readdir(f2, join=true)
     v = []
-    h=[]
+    h=String[]
+
     for file in r
-        l = CSV.File(file; select=[n])
-        column = getproperty(l, l.names[1])
+        l = CSV.File(file; select=n)
+        for s in 1:length(n)
+        column = getproperty(l, l.names[s])
         push!(v, column)
         push!(h,split(basename(file),".")[1])
+        end
+        
     end
 
-    cat = reduce(hcat, v)
+    @show length.(v)
+    @show r
+
+    cat = reduce(hcat, v)[lim_lin, lim_col]
     tbl = Tables.table(cat)
-    CSV.write("$name.CSV", tbl, header=h)
+    @show h
+    folder=pick_folder()
+    CSV.write(joinpath(folder,"$name.CSV"), tbl, header=h[lim_col])
     print(h)
 end
   
-table("filename",2)
+table("filename",[1,5],[1,3,5],[1,5,6,9])
